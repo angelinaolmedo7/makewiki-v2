fd4e1c7ae39c3efc8ce0dce9e16626ca5283c137
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-11-26 15:07:22 -0800

    API

---
+++ api/views.py
@@ -0,0 +1,36 @@
+from rest_framework.generics import ListCreateAPIView, RetrieveDestroyAPIView
+
+from wiki.models import Page
+from api.serializers import PageSerializer
+
+
+class PageList(ListCreateAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
+
+class PageDetail(RetrieveDestroyAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
+
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from django.shortcuts import get_object_or_404
+#
+# from wiki.models import Page
+# from api.serializers import PageSerializer
+#
+#
+# class PageList(APIView):
+#     def get(self, request):
+#         pages = Page.objects.all()[:20]
+#         data = PageSerializer(pages, many=True).data
+#         return Response(data)
+#
+#
+# class PageDetail(APIView):
+#     def get(self, request, pk):
+#         page = get_object_or_404(Page, pk=pk)
+#         data = PageSerializer(page).data
+#         return Response(data)
